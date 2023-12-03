c895342c4b2bb63d6fffc964bf9555748bd33d26
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-12-03 14:09:19 +0100

    Alter styling of the text input with icon.

--- components/TextInputWithIcon.js
+++ components/TextInputWithIcon.js
@@ -4,22 +4,25 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 const TextInputWithIcon = (props) => {
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 1, padding: 10 }}>
-      <Icon name="ios-person" size={20} color="black" /> {/ Adjust the icon name, size, and color /}
+    <View style={{
+      flexDirection: 'row', alignItems: 'center', borderWidth: 1, paddingLeft: 5
+    }}>
+      <Icon name="person-outline" size={20} color="black" />
       <TextInput
         style={{
           fontSize: 16,
           fontWeight: '300',
           width: '88%',
           color: '#757083',
-          opacity: 0.5,
+          // opacity: 0.5,
           height: 60,
 
           padding: 15,
-          borderWidth: 1,
+          borderWidth: 0,
         }}
         value={props.name}
         onChangeText={props.setName}
+        placeholder='Your Name'
       />
     </View>
   );
